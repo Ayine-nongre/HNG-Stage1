b57c42ae14bc663ab1ba35344577ed9a845aa990
Ayine-nongre <eugeneatinbire@gmail.com>
2024-07-01 14:23:24 +0000

    .

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ const app = express()
 dotenv.config()
 
 app.get('/api/hello', async (req, res) => {
-    const url = `https://weatherapi-com.p.rapidapi.com/current.json?q=${ip.address()}`;
+    const ipAdd = req.headers["cf-connecting-ip"] || req.headers['x-forwarded-for'] || req.connection.remoteAddress || req.headers['x-real-ip'] || req.socket.remoteAddress
+
+    const url = `https://weatherapi-com.p.rapidapi.com/current.json?q=${ipAdd}`;
     const options = {
         method: 'GET',
         headers: {
@@ -19,7 +21,7 @@ app.get('/api/hello', async (req, res) => {
         const response = await fetch(url, options);
         const result = await response.json();
         res.status(200).json({
-            "client_ip": ip.address(),
+            "client_ip": ipAdd,
             "location": result.location.name,
             "greeting": `Hello ${req.query.visitor_name}!, the temperature is ${result.current.temp_c} degrees Celcius in ${result.location.name}`
         })
