3b7140019bd89c8266cd8ce3011434e4ee356ef6
Eugene Atinbire <78736543+Ayine-nongre@users.noreply.github.com>
2024-07-07 19:35:31 +0000

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ import { Sequelize } from 'sequelize'
 export const app = express()
 dotenv.config()
 
-const port = process.env.PORT || 3000
+const port = 3000
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
